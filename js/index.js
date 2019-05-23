938edf45ca9467cad75d82edeb8e794244e7ecad
Maks Konyaev <redsun1988@rambler.ru>
2019-05-23 14:15:27 +0300

    walls generator is created

--- js/index.js
+++ js/index.js
@@ -12,8 +12,19 @@ setup = () =>
     context.lineWidth = 3;
     context.strokeStyle = "white";
 
-    ray1 = new Ray(new Point2D(350, 350));
+
+    var i;
+    for (i = 0; i < 5; i++) {
+        
+        walls.push(new Boundary(new Point2D(660 * Math.random() + 30, 660 * Math.random() + 30), new Point2D(660 * Math.random() + 30, 660 * Math.random() + 30)));
+    }
+    //Borderds
     walls.push(new Boundary(new Point2D(30, 10), new Point2D(670, 10)));
+    walls.push(new Boundary(new Point2D(10, 30), new Point2D(10, 670)));
+    walls.push(new Boundary(new Point2D(30, 690), new Point2D(670, 690)));
+    walls.push(new Boundary(new Point2D(690, 30), new Point2D(690, 670)));
+
+    ray1 = new Ray(new Point2D(350, 350));
     rays.push(ray1);
 };
 draw = () => {
@@ -24,7 +35,9 @@ draw = () => {
     });
     rays.forEach((ray) => {
         ray.draw(context);
-        walls.forEach((wall) => {
+        var i;
+        for(i = 0; i < walls.length; i++) {
+            var wall = walls[i];
             var point = ray.cast(wall);
 
             if (point) {
@@ -32,8 +45,14 @@ draw = () => {
                 context.ellipse(point.X, point.Y, 5, 5, Math.PI / 4, 0, 2 * Math.PI);
                 context.stroke();
 
+                context.beginPath();
+                context.moveTo(ray.start.X, ray.start.Y);
+                context.lineTo(point.X, point.Y);
+                context.stroke();
+       
+                break;
             }
-        });
+        }
     });
 };
 
